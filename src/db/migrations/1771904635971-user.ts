1683ffab3cdf8e0d128fc06ce18578ce7b0cba3c
dino0320 <127201712+dino0320@users.noreply.github.com>
2026-02-24 12:54:17 +0900

    Add migration

---
+++ src/db/migrations/1771904635971-user.ts
@@ -0,0 +1,35 @@
+import {
+    MigrationInterface,
+    QueryRunner,
+    Table,
+} from "typeorm"
+
+export class User1771904635971 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "user",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                    },
+                ],
+            }),
+            true,
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("user", true)
+    }
+
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,30 +5,30 @@ import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly userService: UsersService) {}
+  constructor(private readonly usersService: UsersService) {}
   
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+    return this.usersService.create(createUserDto);
   }
   
   @Get()
   findAll() {
-    return this.userService.findAll();
+    return this.usersService.findAll();
   }
   
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+    return this.usersService.findOne(+id);
   }
   
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
+    return this.usersService.update(+id, updateUserDto);
   }
   
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+    return this.usersService.remove(+id);
   }
 }
