00e934b585c44acde6cf69e4f9b226ef3b8067dd
dino0320 <127201712+dino0320@users.noreply.github.com>
2026-02-24 12:22:15 +0900

    Fix Dockerfile

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -6,8 +6,16 @@ PROJECT_PATH=/srv/task-management.com
 
 cd $PROJECT_PATH
 
-# Add the nginx user to the root group for permission access
-usermod -aG root nginx
+# Install npm packages
+if [ "$APP_ENV" = "local" ]; then
+  # nvm is not loaded so load it
+  source ~/.bashrc
+  npm ci
+
+if [ $IS_NPM_BUILT -eq 1 ]; then
+  nest build
+fi
+fi
 
 # Start NGINX
 # By using -g "daemon off;", NGINX runs in the foreground, preventing the container from exiting automatically
